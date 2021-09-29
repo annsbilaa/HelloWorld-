0ca8df4630b8797044d3c7525914b99ab7e941e4
annsbilaa <annisanabilah1111@gmail.com>
2021-09-29 21:34:10 +0700

    add all menu

--- app/src/main/java/com/example/helloworld/activities/MainActivity.java
+++ app/src/main/java/com/example/helloworld/activities/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.helloworld.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
 import com.example.HelloWorld2.R;
 
 public class MainActivity extends AppCompatActivity {
     private CardView cvHead;
     private CardView cvSports;
+    private CardView cvTechno;
+    private CardView cvBusiness;
+    private CardView cvHealth;
+    private CardView cvEntertaiment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
 
-        cvHead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), BeritaUtama.class));
-            }
-        });
-        cvSports.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Olahraga.class));
-            }
-        });
+        cvHead.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), BeritaUtama.class)));
+        cvSports.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Olahraga.class)));
+        cvTechno.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Teknologi.class)));
+        cvBusiness.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Bisnis.class)));
+        cvHealth.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Kesehatan.class)));
+        cvEntertaiment.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Hiburan.class)));
     }
 
     private void initView() {
         cvHead = (CardView) findViewById(R.id.cardHeadLine);
         cvSports = (CardView) findViewById(R.id.cardSports);
+        cvTechno = (CardView) findViewById(R.id.cardTechno);
+        cvBusiness = (CardView) findViewById(R.id.cardBusiness);
+        cvHealth = (CardView) findViewById(R.id.cardHealth);
+        cvEntertaiment = (CardView) findViewById(R.id.cardEnter);
     }
 }
